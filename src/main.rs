a6df8d7b69b0cd3a3794a2f82f28cc14fc6e6664
ya zhou <huyazhou@msn.com>
2025-05-21 15:01:35 +0800

    feat: add GitHub Actions workflow for Rust release and Docker image build; update package name and configuration file paths
    Signed-off-by: ya zhou <huyazhou@msn.com>

--- src/main.rs
+++ src/main.rs
@@ -40,10 +40,11 @@ async fn main() -> Result<()> {
 
 pub async fn run() -> error::Result<()> {
     let config: Config = Figment::new()
-        .merge(figment::providers::Toml::file("Config.toml"))
+        .merge(figment::providers::Toml::file("/etc/kube-eye-export-server/Config.toml"))
+        .merge(figment::providers::Toml::file("./Config.toml"))
         .merge(figment::providers::Env::prefixed("APP_"))
         .extract()
-        .context(FigmentParseSnafu {})?;
+        .context(FigmentParseSnafu)?;
     let server = server::Server::new(config.server);
     server.run(config.typst).await
 }
